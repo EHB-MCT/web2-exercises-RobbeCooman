060775b4f4c6b23852290e5c43ed7965f960d368
RobbeCooman <robbe.cooman@student.ehb.be>
2021-10-07 10:29:29 +0200

    Added fetch link

--- WC2/script.js
+++ WC2/script.js
@@ -1,5 +1,10 @@
 "use strict";
 
-window.onload = (event) => {
     console.log('page is fully loaded');
-};
+
+
+fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+  
