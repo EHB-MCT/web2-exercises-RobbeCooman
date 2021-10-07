f62cd1c5b4f76421ccd0d0e9ce3b97bf0c2e1b87
RobbeCooman <robbe.cooman@student.ehb.be>
2021-10-07 10:50:20 +0200

    Fetched individual pokemons

--- WC2/script.js
+++ WC2/script.js
@@ -1,10 +1,20 @@
 "use strict";
+let pokemons = [];
 
-    console.log('page is fully loaded');
+console.log('page loaded!');
 
 
 fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+      data.results.forEach(poke => {
+          fetch(poke.url)
+          .then(response2 => response2.json())
+          .then(data2 => {
+              pokemons.push(data2);
+          })
+      });
+  });
 
-  
+  console.log(pokemons);
+
