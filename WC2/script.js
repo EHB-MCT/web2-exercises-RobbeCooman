80f800cf001d2c8e553363aa405148e464c1e357
RobbeCooman <robbe.cooman@student.ehb.be>
2021-10-07 11:58:54 +0200

    trying to add pokemons to html

--- WC2/script.js
+++ WC2/script.js
@@ -5,16 +5,38 @@ console.log('page loaded!');
 
 
 fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-  .then(response => response.json())
-  .then(data => {
-      data.results.forEach(poke => {
-          fetch(poke.url)
-          .then(response2 => response2.json())
-          .then(data2 => {
-              pokemons.push(data2);
-          })
-      });
-  });
+    .then(response => response.json())
+    .then(data => {
+        //get individual pokemon
+        data.results.forEach(poke => {
+            fetch(poke.url)
+                .then(response => response.json())
+                .then(data2 => {
+                    pokemons.push(data2);
+                })
+        });
+    });
 
-  console.log(pokemons);
+console.log(pokemons);
 
+function bluidList() {
+
+    let html = '';
+
+    html += `
+  
+        <img class="pokemonImg" src="${element.sprites.front_default}" alt="${element.name}">
+        <p class="pokemonNr">${element.id}</p>
+        <p class="pokemonName">${element.name}</p>
+        <div class="pokemonTypes">
+            ${typeDiv}  
+        </div>
+    `
+
+    document.getElementById("Pokemon").innerHTML += html;
+
+
+
+
+
+}
---
+++ team.js
@@ -0,0 +1,8 @@
+class team {
+    constructor(){
+    
+    }
+    
+    
+    
+    }
