cd7aaf5fd8bc5a352b4ac0805f8b45a93798b093
RobbeCooman <robbe.cooman@student.ehb.be>
2021-10-14 12:29:47 +0200

    Tired makinjg the exercise

--- WC3/a/script.js
+++ WC3/a/script.js
@@ -1,2 +1,17 @@
 "use strict";
 
+let randomNumber = Math.floor(Math.random() * 20) + 1;
+
+console.log(randomNumber)
+
+const inputForm = document.getElementById("form");
+inputForm.addEventListener('submit', Functie);
+
+
+function Functie() {
+    event.preventDefault();
+
+
+    let htmlString = ``;
+    document.getElementById('output').innerHTML = htmlString;
+}
--- WC3/b/script.js
+++ WC3/b/script.js
@@ -1,2 +1,12 @@
 "use strict";
 
+let baseUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=9cac5673&'
+
+window.onload = function () {
+
+    let url = baseUrl + 'i=tt6751668';
+    getData(url).then(result => {
+        console.log(result)
+    });
+};
+
