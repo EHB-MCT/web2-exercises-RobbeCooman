442d6f74d9981eb4b98f5dc52134e57dc59598b3
RobbeCooman <robbe.cooman@student.ehb.be>
2021-10-21 10:56:16 +0200

    add gitignore

--- Technical test/script.js
+++ Technical test/script.js
@@ -2,7 +2,16 @@
 
 console.log('linked')
 
-fetch('')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(err => console.error(err));
+fetch("https://free-nba.p.rapidapi.com/teams", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "free-nba.p.rapidapi.com",
+		"x-rapidapi-key": "844fccd642msh67b258f17b1f872p142b41jsn54140229af72"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
