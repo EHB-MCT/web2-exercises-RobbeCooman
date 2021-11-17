9c2fc8b776d45578620762f195b8b84a2f5ea055
Robbe <robbe.cooman@student.ehb.be>
2021-11-17 23:35:50 +0100

    Update script.js

--- Technical test/script.js
+++ Technical test/script.js
@@ -2,7 +2,13 @@
 
 console.log('linked')
 
-fetch("")
+fetch("https://api-nba-v1.p.rapidapi.com/players/lastName/james", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+		"x-rapidapi-key": "844fccd642msh67b258f17b1f872p142b41jsn54140229af72"
+	}
+})
 .then(response => {
 	console.log(response);
 })
