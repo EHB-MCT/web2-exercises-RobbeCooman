a4cf0b2bce0dbd91ed98b359b35b6d19e84a6bb4
RobbeCooman <robbe.cooman@student.ehb.be>
2021-11-18 13:50:17 +0100

    Deleted credentials

--- WC5/Mongo/index.js
+++ WC5/Mongo/index.js
@@ -1,6 +1,6 @@
 
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://admin:admin@cluster0.rh9dt.mongodb.net/session5?retryWrites=true&w=majority";
+const uri = "";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("session5").collection("boardgames");
